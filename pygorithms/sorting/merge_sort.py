523bc000f43acd22e6dfb26b0080c32bbf2a0b7b
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2020-09-02 19:40:08 +0100

    Feat/merge sort (#23)
    * add: merge sort

---
+++ pygorithms/sorting/merge_sort.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+def merge_sort(array: List[int]) -> List[int]:
+    """ Complexity:
+
+    Time -> O(nlogn)
+    Space -> O(n)
+    """
+    if not array:
+        return []
+
+    if len(array) == 1:
+        return array
+
+    return (
+        merge(
+            merge_sort(array[:len(array) // 2]),
+            merge_sort(array[len(array) // 2:])
+        )
+    )
+
+
+def merge(a: List[int], b: List[int]) -> List[int]:
+    """ Complexity:
+
+    Time -> O(n)
+    Space -> O(n)
+    """
+    if len(b) == 0:
+        return a
+
+    if len(a) == 0:
+        return b
+
+    if a[0] < b[0]:
+        return a[0:1] + merge(a[1:], b)
+
+    return b[0:1] + merge(a, b[1:])
