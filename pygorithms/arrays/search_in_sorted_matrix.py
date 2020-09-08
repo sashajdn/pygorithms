6696be91998f99a360bb6e0ed6a6360868a1aaf7
alexjperkins <alexjperkins@hotmail.co.uk>
2020-09-08 08:46:37 +0000

    add: recursive search in sorted matrix

--- pygorithms/arrays/search_in_sorted_matrix.py
+++ pygorithms/arrays/search_in_sorted_matrix.py
@@ -2,15 +2,14 @@
 Problem Statement:
 
 Given a two dimensional array (a matrix) of distinct integers and a
-target integer, return the precise index for which this target lies;
-otherwise return the array:
+target integer, return the precise index for which this target lies; otherwise return the array:
 
 ```
 [-1, 1]
 ```
 """
 
-
+from collections.abc import Sequence
 from typing import Any, Callable, List
 
 
@@ -34,21 +33,33 @@ def search_in_sorted_matrix(matrix: List[List[int]], target: int) -> List[int]:
     return [-1, -1]
 
 
+# Primitives
 Y = lambda le: (lambda f: f(f))(lambda f: le(lambda x: f(f)(x)))
 rlen = lambda f: lambda x: 1 + f(x[1:]) if x else 0  # recursive length
 head = lambda x: x[0]
 tail = lambda x: x[1:]
-# TODO : make recursive
-coltail = lambda x: list(zip(*list(zip(*x))[:-1]))  # column tail
 cons = lambda a: lambda b: [a] + [b]
+is_atom = lambda x: not (isinstance(x, Sequence) and not isinstance(x, str))
 
 
 def napply(n: int, f: Callable, x: Any) -> Any:
-    # TODO: curry args & use ``Y`` to recurse
     if n == 0:
         return x
 
     return f(napply(n - 1, f, x))
+
+
+def coltail(x: List[List[int]]) -> List[List[int]]:
+    if len(x) == 0:
+        return []
+
+    if not is_atom(head(x)):
+        return cons(coltail(head(x)))(coltail(tail(x)))
+
+    if not tail(x):
+        return []
+
+    return cons(head(x))(coltail(tail(x)))
 
 
 def search_in_sorted_matrix_recursive(
@@ -57,16 +68,17 @@ def search_in_sorted_matrix_recursive(
     """
     Complexity:
 
-        T -> bad
-        S -> bad
+        T -> O(n^2 + m)  <n^2 due to ``rlen``>
+        S -> O(n + m)
     """
     # no multiline lambda in python :(
     def fn(matrix: List[List[int]], i: int, target: int) -> List[int]:
         if len(matrix) == 0:
-            return cons(-1)(-1)
+            return cons(-1)([-1])
 
+        # apply tail ``len(head(matrix)) - 1`` times, and then head
         if head(napply(Y(rlen)(head(matrix)) - 1, tail, head(matrix))) == target:  # NOQA
-            return cons(i)(Y(rlen)(head(matrix)) - 1)
+            return cons(i)([Y(rlen)(head(matrix)) - 1])
 
         if head(napply(Y(rlen)(head(matrix)) - 1, tail, head(matrix))) > target:  # NOQA
             return fn(
